ac2e0fa793485a3a1e0357399166ec2e2a108fe2
Jehy <fate@jehy.ru>
2017-07-13 10:53:06 +0300

    better minify

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
+import '../css/custom/charlist.css';
+import '../css/custom/dots.css';
+import '../css/custom/list1.css';
+import '../css/custom/list2.css';
+import '../css/custom/list3.css';
+import '../css/custom/list4.css';
+import '../css/custom/print.css';
+import '../css/x-editable/bootstrap-editable.css';
+import '../css/x-editable/datetimepicker.css';
+import '../css/x-editable/select2.css';
+import '../css/x-editable/select2-bootstrap.css';
+import '../css/x-editable/typeahead.js-bootstrap.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap-theme.css';
+
 const $ = require('jquery');
 
 window.$ = $;
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,16 @@
-const path               = require('path'),
-      HtmlWebpackPlugin  = require('html-webpack-plugin'),
-      CleanWebpackPlugin = require('clean-webpack-plugin'),
-      webpack            = require('webpack'),
-      ExtractTextPlugin  = require('extract-text-webpack-plugin');
+const path                 = require('path'),
+      HtmlWebpackPlugin    = require('html-webpack-plugin'),
+      CleanWebpackPlugin   = require('clean-webpack-plugin'),
+      webpack              = require('webpack'),
+      ExtractTextPlugin    = require('extract-text-webpack-plugin'),
+      BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = {
   entry: './src/js/index.js',
   plugins: [
+    new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(ru|en)$/), // https://github.com/webpack/webpack/issues/87
+    new BundleAnalyzerPlugin({analyzerMode: 'static', openAnalyzer: false}),
     new ExtractTextPlugin('styles.css'),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
@@ -38,6 +42,7 @@ module.exports = {
     tls: 'empty',
     fs: 'empty',
     net: 'empty',
+    console: true,
   },
   module: {
     rules: [{
